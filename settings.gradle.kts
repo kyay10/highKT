346e17807c2ec6240cf0dcd554bd0c5c8b48030c
Youssef Shoaib <canonballt@gmail.com>
2025-10-07 13:02:17 +0100

    Update workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,14 @@
 @file:Suppress("UnstableApiUsage")
-
 pluginManagement {
     repositories {
         mavenCentral()
         gradlePluginPortal()
     }
-    
+
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention").version("0.10.0")
 }
 
 dependencyResolutionManagement {
